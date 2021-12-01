2017bac16592b31916c8a244729eb8e32156a408
Alejandra Piedrasanta <alesarti@mesoamericana.edu.gt>
2021-12-01 16:09:18 -0600

    formulario y vista

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import MateriaForm
+from cursos.models import Cursa, Materia
 
-# Create your views here.
+def curso_nueva(request):
+    if request.method == "POST":
+        formulario = MateriaForm(request.POST)
+        if formulario.is_valid():
+            pelicula = Materia.objects.create(nombre=formulario.cleaned_data['nombre'], anio = formulario.cleaned_data['anio'])
+            for actor_id in request.POST.getlist('actores'):
+                actuacion = Cursa(actor_id=actor_id, pelicula_id = pelicula.id)
+                actuacion.save()
+            messages.add_message(request, messages.SUCCESS, 'Pelicula Guardada Exitosamente')
+    else:
+        formulario = MateriaForm()
+    return render(request, 'curso/curso_nueva.html', {'formulario': formulario})
